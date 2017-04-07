f488bd19e6444b669b620a5cf06a47d6835ca426
Nick Hamze <me@nickhamze.com>
2017-04-06 22:47:25 -0500

    Remove post related settings from menu screen
    Forgot to remove the settings for linking to posts or categories

--- inc/blogbye.php
+++ inc/blogbye.php
@@ -18,15 +18,15 @@ add_action( 'admin_menu', 'blogbye_remove_admin_submenus', 999 );
 
 /* Remove post related widgets */
 function blogbye_remove_widgets() {
-	unregister_widget('WP_Widget_Calendar');
-  unregister_widget('WP_Widget_Archives');
-  unregister_widget('WP_Widget_Categories');
-  unregister_widget('WP_Widget_Recent_Posts');
-  unregister_widget('WP_Widget_Recent_Comments');
-  unregister_widget('WP_Widget_RSS');
-  unregister_widget('WP_Widget_Meta');
-  unregister_widget('WP_Widget_Search');
-  unregister_widget('WP_Widget_Tag_Cloud');
+	unregister_widget( 'WP_Widget_Calendar' );
+  unregister_widget( 'WP_Widget_Archives' );
+  unregister_widget( 'WP_Widget_Categories' );
+  unregister_widget( 'WP_Widget_Recent_Posts' );
+  unregister_widget( 'WP_Widget_Recent_Comments' );
+  unregister_widget( 'WP_Widget_RSS' );
+  unregister_widget( 'WP_Widget_Meta' );
+  unregister_widget( 'WP_Widget_Search' );
+  unregister_widget( 'WP_Widget_Tag_Cloud' );
 }
 add_action( 'widgets_init', 'blogbye_remove_widgets' );
 
@@ -39,12 +39,12 @@ add_action( 'admin_bar_menu', 'blogbye_remove_post_link', 999 );
 
 /* Remove post related dashboard widgets */
 function blogbye_remove_dashboard_meta() {
-  remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
-  remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
-  remove_meta_box('dashboard_activity', 'dashboard', 'side');
-  remove_meta_box('dashboard_right_now', 'dashboard', 'side');
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_activity', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_right_now', 'dashboard', 'side' );
 }
-add_action('admin_init', 'blogbye_remove_dashboard_meta');
+add_action( 'admin_init', 'blogbye_remove_dashboard_meta' );
 
 /* Set permalinks to post name */
 add_action( 'init', function() {
@@ -61,9 +61,17 @@ add_action( 'after_setup_theme', 'blogbye_after_setup_theme' );
 
 /* Update customizer setting to only show page stuff */
 function blogbye_remove_settings( $wp_customize ) {
-  $wp_customize->remove_control('show_on_front');
-  $wp_customize->remove_control('page_for_posts');
+  $wp_customize->remove_control( 'show_on_front' );
+  $wp_customize->remove_control( 'page_for_posts' );
+  $wp_customize->remove_control( 'available-menu-items-post_type-post' );
 }
 add_action( 'customize_register', 'blogbye_remove_settings' );
+
+/* Remove post stuff from custom menus */
+function blogbye_remove_nav_fields() {
+  remove_meta_box( 'add-post-type-post', 'nav-menus', 'side' );
+  remove_meta_box( 'add-category', 'nav-menus', 'side' );
+}
+add_action( 'admin_head-nav-menus.php' , 'blogbye_remove_nav_fields' );
 
 ?>
